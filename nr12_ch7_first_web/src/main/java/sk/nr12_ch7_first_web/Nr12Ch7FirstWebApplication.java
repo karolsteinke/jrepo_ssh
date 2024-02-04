3e895dda3251ac1327127ca22cf48f2352c62dc1
karol.steinke <karol.steinke@gmail.com>
2024-02-04 18:33:05 +0100

    Spring Initializr for nr12 used

---
+++ nr12_ch7_first_web/src/main/java/sk/nr12_ch7_first_web/Nr12Ch7FirstWebApplication.java
@@ -0,0 +1,13 @@
+package sk.nr12_ch7_first_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nr12Ch7FirstWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nr12Ch7FirstWebApplication.class, args);
+	}
+
+}
---
+++ nr12_ch7_first_web/src/test/java/sk/nr12_ch7_first_web/Nr12Ch7FirstWebApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.nr12_ch7_first_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nr12Ch7FirstWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
